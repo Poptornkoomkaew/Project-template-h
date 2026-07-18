2abfd10a20dd683567c26f0aac9cd507f140c1c4
Student <Student@Train11-3-PC183.control.ad.sit.kmutt.ac.th>
2026-07-18 11:09:32 +0700

    search_member

--- personnel/search_member.py
+++ personnel/search_member.py
@@ -1,11 +1,18 @@
 # =====================================================
-#  personnel/search_member.py — คนรับผิดชอบ: ______________________
+#  personnel/search_member.py — คนรับผิดชอบ: Mj
 # =====================================================
 from data import family_members
 
 # TODO: สร้างฟังก์ชัน search_member(target_name)
 #   - หาคนใน family_members ที่ชื่อตรงกับ target_name (ไม่สนตัวพิมพ์ใหญ่/เล็ก)
 #   - เจอ -> return dict ของคนนั้น | ไม่เจอ -> return None
+def search_member(target_name) :
+    for person in family_members :
+        if target_name.lower() == person["name"].lower() :
+            return person["name"]
+        else :
+            return None
+            
 
 
 # ทดสอบ: python -m personnel.search_member
