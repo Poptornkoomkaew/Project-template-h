b10f59719f25bbc7454eb0813ec1f33422a8b792
BallParadorn <paradorn11374@gmail.com>
2026-07-18 11:58:34 +0700

    remove

--- personnel/remove_member.py
+++ personnel/remove_member.py
@@ -6,7 +6,14 @@ from data import family_members
 # TODO: สร้างฟังก์ชัน remove_member(target_name)
 #   - หาคนที่ชื่อตรงกับ target_name (ไม่สนตัวพิมพ์ใหญ่/เล็ก) แล้วลบออกจาก family_members
 #   - ลบสำเร็จ -> return True | ไม่เจอ -> return False
-
+def remove_member (target_name):
+    x = target_name.lower()
+    for i in family_members : 
+        m = i["name"].lower()
+        if x == m:
+            family_members.remove(i)
+            return True
+    return False
 
 # ทดสอบ: python -m personnel.remove_member
 if __name__ == "__main__":
